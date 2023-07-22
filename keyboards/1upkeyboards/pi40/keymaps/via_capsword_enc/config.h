f8974f9f07b88f73d7611b8329549278155928db
GuestJim <30958420+GuestJim@users.noreply.github.com>
2023-07-22 08:54:43 -0400

    My pi40 keymaps
    This is likely out of date with latest version of QMK

---
+++ keyboards/1upkeyboards/pi40/keymaps/via_capsword_enc/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define TAPPING_TERM 150
+//	default is 200
+
+#define RETRO_TAPPING
+//	with this the TAP command is sent if no other input is provided during the hold
+//	my thinking is this can cover for when the tapping term might be too short
